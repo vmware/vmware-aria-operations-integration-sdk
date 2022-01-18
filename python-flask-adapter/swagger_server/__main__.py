06a63e76e96e0a971902fba01622ce9cd8fd095e
Kyle Rokos <krokos@vmware.com>
2022-01-18 11:14:44 -0500

    Switch to using production HTTP server

--- python-flask-adapter/swagger_server/__main__.py
+++ python-flask-adapter/swagger_server/__main__.py
@@ -9,7 +9,13 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Adapter API'}, pythonic_params=True, validate_responses=True)
-    app.run(port=8080)
+
+    # development server
+    # app.run(port=8080)
+
+    # production server
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
 
 if __name__ == '__main__':
