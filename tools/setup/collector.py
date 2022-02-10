829af6ca38fb832ee6f05b4419135911f39779d8
Santiago Quiroga Cubillos <squirogacubi@vmware.com>
2022-02-10 10:03:40 -0700

    collector class

--- tools/setup/collector.py
+++ tools/setup/collector.py
@@ -1,106 +1,147 @@
 import sys
 import os
 import json
+import psutil
+
+
+
+class Collector:
+    def __init__(self):
+        #get connection parameters
+        print("initializing collector")
+        self.connect()
+
+    def connect(self):
+        #connect to service/other
+        print("connecting...")
+
+    def test(self):
+        #ensure that there is communication  somehow
+        print("Python test")
+
+
+    def collect(self):
+        #container statistics
+        print("collecting")
+        cpu_percent = psutil.cpu_percent(1)
+        print(f"cpu percent used: {cpu_percent}")
+
+
+#class Metric:
+#    def __init__(self, key: str, value, int):
+#        self.key = key
+#        self.value = value
+#        self.timestamp = timestamp
+#
+#    def to_string(self):
+#        return {
+#                "key": self.key,
+#                "numberValue": self.value,
+#                "timestamp": self.timestamp
+#                }
+#
+#
+#
+#class Property:
+#    def __init__ (self, key: str, value ):
+#        #TODO: parse value and check whether is a string or a number
+#          self.key = key
+#          self.value = value,
+#          self.timestamp = -1
+#
+#    def to_string(self):
+#        return {
+#                "key": self.key,
+#                "stringValue": self.value,
+#                "numberValue": self.value,
+#                "timestamp": self.timestamp
+#                }
+#
+#
+#class Event:
+#    def __init__(self, criticality: str, message: str, faultKey, autocancel = False,  startDate, updateDate, cancelDate, watchWaitCyccle, cancelWaitCycle):
+#        self.criticality = criticality
+#        self.message = message
+#        self.faultkey = faultkey
+#        self.autocancel = autocancel
+#        self.startDate = startDate
+#        self.updateDate = updateDate
+#        sefl.cancelDate = cancelDate
+#        self.watchWaitCycle = watchWaitCycle
+#        self.cancelWaitCycle = cancelWaitCycle
+#
+#
+#    def to_string(self):
+#        return {
+#                "criticality": self.criticality,
+#                "message": self.message,
+#                "faultKey": self.faultkey,
+#                "autoCancel": self.autocancel,
+#                "startDate": self.startDate,
+#                "updateDate": self.updateDate,
+#                "cancelDate": sefl.cancelDate,
+#                "watchWaitCycle": self.watchWaitCycle,
+#                "cancelWaitCycle": self.cancelWaitCycle
+#                }
+#
+#class Identifier:
+#    def __init__(self, key: str, value: str, isPartOfUniqueness: bool):
+#        #TODO constructor
+#
+#class Object:
+#    def __init__(self, name: str, adapterKind: str, bojectKind: str, identifiers:[], ):
+#
+#        def object():
+#            {
+#                    "result": [
+#                        {
+#                            "key": {
+#                                #Object
+#                                },
+#                            "metrics": [
+#                                #Metric
+#                                ],
+#                            "properties": [
+#                                #Property
+#                                ],
+#                            "events": [
+#                                #Event
+#                                ]
+#                            }
+#                        ],
+#                    "relationships": [
+#                        {
+#                            "parent": {
+#                                #Object
+#                                },
+#                            "children": [
+#                                {
+#                                    #Object
+#                                    }
+#                                ]
+#                            }
+#                        ],
+#                    "notExistingObjects": [
+#                        {
+#                            #Object
+#                            }
+#                        ],
+#                    "errorMessage": "string"
+#                    }
+
 
 def main(argv):
+    collector = Collector()
     if len(argv) == 0:
         print("No arguments")
     elif argv[0] in 'collect':
-        print("Python collect")
-        //collect()
+        #collect()
+        collector.collect()
     elif argv[0] in 'test':
-        print("Python test")
-        test()
+        #test()
+        collector.test()
     else:
         print(f"Command {argv[0]} not found")
-
-
-def connect():
-
-def test():
-   //ensure that there is communication  somehow
-
-def collect():
-    //container statistics
-    cpu_percent = psutil.cpu_percent(1))
-
-
-class Metric:
-    def __init__(self, key: str, value, int):
-        #TODO finish constructor
-        return  {"key": "string","numberValue": 0,"timestamp": -1}
-
-class Property:
-   def __init__ (self, key: str, value ):
-    #TODO: parse value and check whether is a string or a number
-        {
-          "key": "string",
-          "stringValue": "string",
-          "numberValue": 0,
-          "timestamp": -1
-        }
-
-class Event:
-    def __init__(self, criticality: str, message: str, startDate, updateDate, cancelDate, watchWaitCyccle, cancelWaitCycle):
-        #TODO constructor
-        {
-          "criticality": 0,
-          "message": "string",
-          "faultKey": "string",
-          "autoCancel": false,
-          "startDate": 0,
-          "updateDate": 0,
-          "cancelDate": 0,
-          "watchWaitCycle": 1,
-          "cancelWaitCycle": 3
-        }
-
-class Identifier:
-    def __init__(self, key: str, value: str, isPartOfUniqueness: bool):
-        #TODO constructor
-
-class Object:
-    def __init__(self, name: str, adapterKind: str, bojectKind: str, identifiers:[], ):
-
-def object():
-    {
-  "result": [
-    {
-      "key": {
-      #Object
-      },
-      "metrics": [
-      #Metric
-      ],
-      "properties": [
-      #Property
-      ],
-      "events": [
-      #Event
-      ]
-    }
-  ],
-  "relationships": [
-    {
-      "parent": {
-         #Object
-      },
-      "children": [
-        {
-          #Object
-        }
-      ]
-    }
-  ],
-  "notExistingObjects": [
-    {
-        #Object
-    }
-  ],
-  "errorMessage": "string"
-}
-
-
 
 if __name__ == '__main__':
     main(sys.argv[1:])
