397f4a21bbf14509687175d8f46bcbf319b51897
Kyle Rokos <krokos@vmware.com>
2022-03-21 10:34:03 -0400

    Set up server logging

--- http-server/swagger_server/__main__.py
+++ http-server/swagger_server/__main__.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
 import connexion
+import logging
 
 from swagger_server import encoder
 
 
 def main():
+    try:
+        logging.basicConfig(filename='/var/log/server.log',
+                            filemode='a',
+                            format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                            datefmt='%H:%M:%S',
+                            level=logging.DEBUG)
+        logger = logging.getLogger('waitress')
+        logger.setLevel(logging.DEBUG)
+    except Exception as e:
+        logging.basicConfig(level=logging.CRITICAL+1)
+
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Adapter API'}, pythonic_params=True, validate_responses=True)
