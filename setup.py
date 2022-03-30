4ae86a8a7fb0989bd59d86cb7534e092028057d6
Santiago Quiroga Cubillos <squirogacubi@vmware.com>
2022-03-30 15:30:47 -0500

    Add error message

--- setup.py
+++ setup.py
@@ -5,7 +5,15 @@ from setuptools import setup
 
 import tools.common.filesystem as filesystem
 
-filesystem.get_root_directory(default_path=lambda: os.getenv("REPO_PATH"))
+repo_path = os.getenv("VROPS_SDK_REPO_PATH")
+
+if repo_path is None:
+    print("VROPS_SDK_REPO_PATH not found")
+    print("To install this package run bin/install")
+    print("Alternatively set an environment variable VROPS_SDK_REPO_PATH with the path to this vrops-python-sdk repo")
+    exit(1)
+else:
+    filesystem.get_root_directory(default_path=lambda: repo_path)
 
 setup(
     name="vrops-adapter-tools",
