21b924196bed5f97e33efd70735661ee1085bf8b
Santiago Quiroga Cubillos <squirogacubi@vmware.com>
2022-04-29 08:55:23 -0500

    login solution

--- tools/common/docker.py
+++ tools/common/docker.py
@@ -1,16 +1,14 @@
-from common.config import get_config_values
+import subprocess
+
+from common.config import get_config_value
 
 
 def login(docker_client):
     # This is where we should get the credentials from the config file, and they aren't present
     # we should ask the user for them, and update the config file.
-    docker_login_config = get_config_values("username", "password", "registry_url",
-                                            defaults={"registry_url": "harbor-repo.vmware.com"})
+    docker_registry = get_config_value("registry_url", default="harbor-repo.vmware.com")
 
-    docker_client.login(
-        username=docker_login_config["username"],
-        password=docker_login_config["password"],
-        registry=docker_login_config["registry_url"]
-    )
+    print(f"login into {docker_registry}")
+    subprocess.run(["docker", "login", f"{docker_registry}"])
 
-    return docker_login_config["registry_url"]
+    return docker_registry
