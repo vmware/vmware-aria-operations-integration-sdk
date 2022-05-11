d2799a45e33a5b9423ca93ce1ef1002a59f352c0
Santiago Quiroga Cubillos <squirogacubi@vmware.com>
2022-05-11 16:55:51 -0600

    Add TODOs

--- tools/common/docker_wrapper.py
+++ tools/common/docker_wrapper.py
@@ -35,25 +35,65 @@ def init():
         return client
     except docker.errors.DockerException as e:
         if "ConnectionRefusedError" in e.args[0]:
-            raise  DaemonError("Cannot connect to the Docker daemon at unix:///var/run/docker.sock. Is the docker daemon running?")
+            print("Cannot connect to the Docker daemon at unix:///var/run/docker.sock. Is the docker daemon running?")
         elif "PermissionError" in e.args[0]:
-            raise DockerGroupError(f"Cannoct run docker commands. Make sure the user {os.getlogin()} has permisions to run docker")
-
+            print(f"Cannoct run docker commands. Make sure the user {os.getlogin()} has permisions to run docker")
         else:
-            raise InitFailure(f"Unexpected error when stablishing connection with Docker daemon: {e}")
+            print(f"Unexpected error when stablishing connection with Docker daemon: {e}")
+
+        raise InitError
+
+def push_image(client, image_tag):
+    """
+    Pushes the given image tag and returns the images digest.
+
+    If there is an error during while pushing the image, then it will gerenate a
+    PushError, that the user can handle
+
+    NOTE: An alternate method of parsing the digest from an image would be to
+    parse the attributes of an image and then check if the image has repoDigests
+    attribute, then we could parse the repo digest (different from digest) to get the digest.
 
 
-class LoginFailure(Exception):
-    """Raised when the subprocess fails"""
-    pass
+    :param client: the docker client that comunicates to the docker daemon
+    :param image_tag: An image tag that identifies the image to be pushed
+    :return: A string version of the SHA256 digest
+    """
+    response = client.images.push(image_tag, stream=True, decode=True)
 
-class InitFailure(Exception):
+    image_digest = ""
+
+    for line in response:
+        if 'aux' in line:
+            try:
+                image_digest = line['aux']['Digest']
+            except KeyError:
+                print(f"Image Digest was not found in response from server")
+                raise PushError
+
+        elif 'errorDetail' in line:
+            print(f"{line['errorDetail']['message']}")
+            raise PushError
+
+    return image_digest
+
+def build_image(client, path, tag):
+    try:
+        return  client.images.build(path=path, tag=tag, nocache=True, rm=True)
+    except docker.errors.BuildError as error:
+        print(f"An error ocurred while trying to build docker image at {path}")
+        print(error)
+        raise BuildError
+
+
+class InitError(Exception):
     """Raise for any unzatgorized errors during init"""
     pass
 
-class DaemonError(Exception):
-    """Raised when connection is refused when trying to connect to Docker daemon"""
+class PushError(Exception):
+    """Raised when the registry server sends back an error"""
     pass
 
-
-print(f"client: {init()}")
+class BuildError(Exception):
+    """Raised when and error occurs while building the Docker image"""
+    pass
