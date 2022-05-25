361e9976a852d4e6c27e0aa67951cb16fbadebb2
Santiago Quiroga Cubillos <squirogacubi@vmware.com>
2022-05-25 14:11:26 -0500

    Finishing touches

--- install.bat
+++ install.bat
@@ -1,15 +1,15 @@
-:: maybe use setx instead
 set VIRTUAL_ENV_FILE_NAME=vrops_mp_sdk_venv
 
 set "VROPS_SDK_REPO_PATH=%cd%"
 
 :: Create virtual environment
-echo "Creating python virtual environament"
+echo "Creating python virtual environment"
 python -m venv %VIRTUAL_ENV_FILE_NAME%
 
-echo "Installing Python dependencies and tooling in virtual environament"
+echo "Installing Python dependencies and tooling in virtual environment"
 :: Activate virtual environment to source
 call .\%VIRTUAL_ENV_FILE_NAME%\Scripts\activate.bat; pip install .
 
+:: Deactivate virtual environment, so the user experience is the same for both platforms
+call deactivate
 echo "Run activate.bat script located at %VROPS_SDK_REPO_PATH%/Scripts/activate.bat"
-
