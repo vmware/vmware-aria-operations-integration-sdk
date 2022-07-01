647ac595ef75d79a179890f476e60691e03c0bc3
Kyle Rokos <krokos@vmware.com>
2022-07-01 15:51:56 -0400

    Update and expand comments in adapter template

--- tools/templates/adapter.py
+++ tools/templates/adapter.py
@@ -15,21 +15,33 @@ logger = logging.getLogger(__name__)
 def test(adapter_instance: AdapterInstance) -> TestResult:
     result = TestResult()
     try:
+        # Sample test connection code follows. Replace with your own test connection code.
+        # A typical test connection will generally consist of:
+        # 1. Read identifier values from adapter_instance that are required to
+        #    connect to the target(s)
+        # 2. Connect to the target(s), and retrieve some sample data
+        # 3. Disconnect cleanly from the target (ensure this happens even if an error occurs)
+        # 4. If any of the above failed, return an error, otherwise pass.
+
         # Read the 'ID' identifier in the adapter instance and use it for a
-        # connection test. In this case the adapter does not need to connect
-        # to anything, as it reads directly from the host it is running on.
-        # Thus, as there is not an actual failure condition to test for, this
-        # example only shows the mechanics of reading identifiers and
-        # constructing test results.
+        # connection test.
         id = adapter_instance.get_identifier_value("ID")
+
+        # In this case the adapter does not need to connect
+        # to anything, as it reads directly from the host it is running on.
         if id is None:
             result.with_error("No ID Found")
         elif id.lower() == "bad":
+            # As there is not an actual failure condition to test for, this
+            # example only shows the mechanics of reading identifiers and
+            # constructing test results. Here we add an error to the result
+            # that is returned.
             result.with_error("The ID is bad")
         # otherwise, the test has passed
         return result
 
     except Exception as e:
+        # TODO: If any connections are still open, make sure they are closed before returning
         logger.error("Unexpected connection test error")
         logger.exception(e)
         result.with_error("Unexpected connection test error: " + repr(e))
@@ -38,11 +50,23 @@ def test(adapter_instance: AdapterInstance) -> TestResult:
 
 def get_endpoints(adapter_instance: AdapterInstance) -> EndpointResult:
     result = EndpointResult()
-    # Add each URL that the adapter uses. Often this will be derived from a 'host'
+    # In the case that an SSL Certificate is needed to communicate to the target,
+    # add each URL that the adapter uses here. Often this will be derived from a 'host'
     # parameter in the adapter instance. In this Adapter we don't use any HTTPS
     # connections, so we won't add any. If we did, we might do something like this:
+    # result.with_endpoint(adapter_instance.get_identifier_value("host"))
+    #
+    # Multiple endpoints can be returned, like this:
     # result.with_endpoint(adapter_instance.get_identifier_value("primary_host"))
     # result.with_endpoint(adapter_instance.get_identifier_value("secondary_host"))
+    #
+    # This 'get_endpoints' method will be run before the 'test' method,
+    # and vROps will use the results to extract a certificate from each URL. If the
+    # certificate is not trusted by the vROps Trust Store, the user will be prompted
+    # to either accept or reject the certificate. If it is accepted, the certificate
+    # will be added to the AdapterInstance object that is passed to the 'test' and
+    # 'collect' methods. Any certificate that is encountered in those methods should
+    # then be validated against the certificate(s) in the AdapterInstance.
     return result
 
 
@@ -50,6 +74,15 @@ def collect(adapter_instance: AdapterInstance) -> CollectResult:
     result = CollectResult()
     try:
         logger.debug("Starting collection")
+        # Sample collection code follows. Replace this with your own collection code.
+        # A typical collection will generally consist of:
+        # 1. Read identifier values from adapter_instance that are required to
+        #    connect to the target(s)
+        # 2. Connect to the target(s), and retrieve data
+        # 3. Add the data into a CollectResult's objects, properties, metrics, etc
+        # 4. Disconnect cleanly from the target (ensure this happens even if an error occurs)
+        # 5. Return the CollectResult.
+
         # CPU
         cpu = result.object(ADAPTER_KIND, "CPU", "CPU")
         # properties
@@ -103,12 +136,14 @@ def collect(adapter_instance: AdapterInstance) -> CollectResult:
         logger.debug(f"Returning collection result {result}")
         return result
     except Exception as e:
+        # TODO: If any connections are still open, make sure they are closed before returning
         logger.error("Unexpected collection error")
         logger.exception(e)
         result.with_error("Unexpected collection error: " + repr(e))
         return result
 
 
+# Main entry point of the adapter. You should not need to modify anything below this line.
 def main(argv):
     try:
         logging.basicConfig(filename="/var/log/adapter.log",
