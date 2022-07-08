1b4c8ad2964de5d53bcfb1cd15a0d21c98510693
Kyle Rokos <krokos@vmware.com>
2022-07-08 13:26:43 +0000

    Additional metric validation

---
+++ tools/common/describe.py
@@ -0,0 +1,42 @@
+import os
+
+import xml.etree.ElementTree as ET
+
+
+def get_describe(path):
+    return ET.parse(os.path.join(path, "conf", "describe.xml")).getroot()
+
+
+def ns(kind):
+    return "{http://schemas.vmware.com/vcops/schema}" + kind
+
+
+def get_adapter_kind(describe):
+    # TODO: if we get more than one adapter kind then we should considered it an error
+    return describe.get("key")
+
+
+def get_adapter_instance(describe):
+    adapter_instance_kind = None
+
+    for resource_kind in get_resource_kinds(describe):
+        if resource_kind.get("type") == "7":
+            adapter_instance_kind = resource_kind
+
+    return adapter_instance_kind
+
+
+def get_resource_kinds(describe):
+    return describe.find(ns("ResourceKinds")).findall(ns("ResourceKind"))
+
+
+def get_identifiers(resource_kind):
+    return resource_kind.findall(ns("ResourceIdentifier"))
+
+
+def get_credential_kinds(describe):
+    credential_kinds = describe.find(ns("CredentialKinds"))
+    if credential_kinds is None:
+        return None
+    else:
+        return credential_kinds.findall(ns("CredentialKind"))
