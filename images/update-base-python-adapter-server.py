436460bd37d911f946670361d8e046883bb693e8
Kyle Rokos <krokos@vmware.com>
2022-10-12 09:06:22 -0400

    Improve project structure (#36)
    * Rename and relocate adapter images
    * Add missing 'enum_values' attribute in definition endpoint
    * Add tool to combine swagger documents and regenerate python server

---
+++ images/update-base-python-adapter-server.py
@@ -0,0 +1,37 @@
+#  Copyright 2022 VMware, Inc.
+#  SPDX-License-Identifier: Apache-2.0
+import json
+import os.path
+
+from vrealize_operations_integration_sdk.docker_wrapper import init
+
+
+def combine_swagger_files(base, extra):
+    with open(base, "r") as base_file:
+        base_json = json.load(base_file)
+
+    with open(extra, "r") as extra_file:
+        extra_json = json.load(extra_file)
+
+    for attribute in extra_json["paths"]:
+        base_json["paths"][attribute] = extra_json["paths"][attribute]
+    for attribute in extra_json["components"]["schemas"]:
+        base_json["components"]["schemas"][attribute] = extra_json["components"]["schemas"][attribute]
+
+    return base_json
+
+
+def main():
+    combined = combine_swagger_files(
+        os.path.join("..", "vrealize_operations_integration_sdk", "api", "vmware-aria-operations-collector-fwk2.json"),
+        os.path.join("..", "vrealize_operations_integration_sdk", "api", "integration-sdk-definition-endpoint.json"))
+    with open("combined.json", "w") as combined_file:
+        json.dump(combined, combined_file, indent=3)
+
+    docker_client = init()
+    image = docker_client.images.pull("swaggerapi/swagger-codegen-cli-v3")
+    docker_client.containers.run(image.id, ["generate", "-i", "local/combined.json", "-l", "python-flask", "-o", "local/base-python-adapter"], remove=True, volumes={f"{os.getcwd()}": {"bind": "/local", "mode": "rw"}})
+
+
+if __name__ == "__main__":
+    main()
