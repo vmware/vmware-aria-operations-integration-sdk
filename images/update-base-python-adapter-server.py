415f9ff0fda065aaec6d68d49fb0faebcb1b3f0c
Kyle Rokos <krokos@vmware.com>
2022-11-18 10:58:23 -0500

    Fix incorrect imports in 'images' directory scripts

--- images/update-base-python-adapter-server.py
+++ images/update-base-python-adapter-server.py
@@ -3,7 +3,7 @@
 import json
 import os.path
 
-from vmware_aria_operations_integration_sdk import init
+from vmware_aria_operations_integration_sdk import docker_wrapper
 
 
 def combine_swagger_files(base, extra):
@@ -23,12 +23,12 @@ def combine_swagger_files(base, extra):
 
 def main():
     combined = combine_swagger_files(
-        os.path.join("..", "vrealize_operations_integration_sdk", "api", "vmware-aria-operations-collector-fwk2.json"),
-        os.path.join("..", "vrealize_operations_integration_sdk", "api", "integration-sdk-definition-endpoint.json"))
+        os.path.join("..", "vmware_aria_operations_integration_sdk", "api", "vmware-aria-operations-collector-fwk2.json"),
+        os.path.join("..", "vmware_aria_operations_integration_sdk", "api", "integration-sdk-definition-endpoint.json"))
     with open("combined.json", "w") as combined_file:
         json.dump(combined, combined_file, indent=3)
 
-    docker_client = init()
+    docker_client = docker_wrapper.init()
     image = docker_client.images.pull("swaggerapi/swagger-codegen-cli-v3")
     docker_client.containers.run(image.id, ["generate", "-i", "local/combined.json", "-l", "python-flask", "-o", "local/base-python-adapter"], remove=True, volumes={f"{os.getcwd()}": {"bind": "/local", "mode": "rw"}})
 
