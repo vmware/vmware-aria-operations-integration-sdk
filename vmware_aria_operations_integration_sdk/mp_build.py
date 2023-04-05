11465fad9066d0df18ed80b1065617a42c70374b
Santiago Quiroga Cubillos <squirogacubi@vmware.com>
2023-04-05 14:50:30 -0500

    Move default_registry to global config

--- vmware_aria_operations_integration_sdk/mp_build.py
+++ vmware_aria_operations_integration_sdk/mp_build.py
@@ -135,7 +135,6 @@ def registry_prompt(default: str) -> str:
         "domain: projects.registry.vmware.com\n"
         "port: 443\n"
         "path: vmware_aria_operations_integration_sdk_mps/base-adapter\n"
-        "Domain is optional, and defaults to Docker Hub (docker.io)\n"
         "Port number is optional, and defaults to 443.",
     )
 
@@ -147,7 +146,8 @@ def get_docker_registry(
     **kwargs: Any,
 ) -> str:
     docker_registry = get_config_value("docker_registry", config_file=config_file)
-    default_registry_value = f"harbor-repo.vmware.com/vmware_aria_operations_integration_sdk_mps/{adapter_kind_key.lower()}"
+    default_registry_value = get_config_value("default_docker_registry")
+
 
     original_value = docker_registry
     if docker_registry is None and docker_registry_arg is None:
@@ -155,7 +155,12 @@ def get_docker_registry(
             "mp-build needs to configure a container registry to store the adapter container image.",
             "class:information",
         )
-        docker_registry = registry_prompt(default=default_registry_value)
+        if default_registry_value is not None:
+        # The default registry should have a trailing fordward slash
+            default_registry_value = f"{default_registry_value}{adapter_kind_key.lower()}"
+            docker_registry = registry_prompt(default=default_registry_value)
+        else:
+            docker_registry = registry_prompt(default="")
 
         first_time = True
         while not is_valid_registry(docker_registry):
